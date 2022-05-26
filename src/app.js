b2f753e0628fac9d4d0b09eeefa2338dbf3cce5f
Genaro_Mazur <genaromazur03@gmail.com>
2022-05-26 20:15:46 -0300

    [mod app.js]

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express=require("express");
 const app=express()
 const path=require("path")
 
-app.listen(3001,()=>{
-    console.log("Servidor lanzado en puerto 3001")
+// app.listen(process.env.PORT || 3001,()=>{
+//     console.log("Servidor lanzado en puerto 3001")
+// })
+app.listen(app.get("puerto"),()=>{
+    console.log('servidor corriedo en ${app.get("puerto")}')
 })
-
 app.use(express.static(path.join(__dirname,"./../public")))
+app.set("puerto",process.env.PORT || 3001)
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"))
